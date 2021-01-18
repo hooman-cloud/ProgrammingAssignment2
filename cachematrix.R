f4388f4ab193aac32ce519155bff2117e6ad5d55
hooman-cloud <farsani@asu.edu>
2021-01-18 13:20:44 -0500

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        inverse <- NULL
+        set <- function(y){
+                
+                x <<- y
+                inverse <<- NULL
+                
+        }
+        get <- function() x
+        setinv <- function(solve) inverse <<- solve
+        getinv <- function() inverse
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 
 }
 
@@ -12,4 +24,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinv()
+        if (!is.null(inverse)) {
+                
+                message("getting cached data")
+                return(inverse) 
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinv(inverse)
+        inverse
+                
 }
